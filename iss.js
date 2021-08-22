2886cf04c3c81cf5f4f8a8e323a225df35df17b7
RiazK <RiazCKhan@gmail.com>
2021-08-22 17:04:40 -0400

    phase 2 fetch coords complete

--- iss.js
+++ iss.js
@@ -1,9 +1,3 @@
-//  * Makes a single API request to retrieve the user's IP address.
-//  * Input:
-//  *   - A callback (to pass back an error or the IP string)
-//  * Returns (via Callback):
-//  *   - An error, if any (nullable)
-//  *   - The IP address as a string (null if error). Example: "162.245.144.188"
 const request = require('request');
 
 const fetchMyIP = function(callback) {
@@ -22,10 +16,23 @@ const fetchMyIP = function(callback) {
 };
 
 const fetchCoordsByIP = (ip, callback) => {
+  request("https://freegeoip.app/json/", function(error, response, body) {
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
 
-}
+    const { latitude, longitude } = JSON.parse(body);
 
-module.exports = { 
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = {
   fetchMyIP,
-  fetchCoordsByIP 
- };
+  fetchCoordsByIP
+};
