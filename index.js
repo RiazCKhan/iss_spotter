d2438889fd7ff75bb3c158624e5288332cd53d7c
RiazK <RiazCKhan@gmail.com>
2021-08-23 19:20:27 -0400

    aded fetchISS flyover times callback

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 // index.js
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
   
-  console.log('It worked! Returned IP:' , ip);
-});
+//   console.log('It worked! Returned IP:' , ip);
+// });
 
-fetchCoordsByIP('76.69.85.78', (error, coordinates) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchCoordsByIP('76.69.85.78', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned coordinates:' , coordinates);
-});
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
 
-// fetchISSFlyOverTimes()
+// fetchISSFlyOverTimes({ latitude: '44.4406', longitude: '-79.6676'}, (error, time) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned Fly Over Times' , time);
+// });
