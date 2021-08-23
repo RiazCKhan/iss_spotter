6609464bac381f9374fd8f89162f8456084db2cb
RiazK <RiazCKhan@gmail.com>
2021-08-23 18:54:28 -0400

    updated index fetchCoordsByIP, IP string

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 // index.js
 const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
   
-//   console.log('It worked! Returned IP:' , ip);
-// });
+  console.log('It worked! Returned IP:' , ip);
+});
 
+fetchCoordsByIP('76.69.85.78', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
 
-// fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
 
-//   console.log('It worked! Returned coordinates:' , coordinates);
-// });
+// fetchISSFlyOverTimes()
