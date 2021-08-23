06c8ced7a9634e61ca4c2e255518903293ff5550
RiazK <RiazCKhan@gmail.com>
2021-08-23 19:21:30 -0400

    added fetch ISS flyover Time function - requires more testing

--- iss.js
+++ iss.js
@@ -32,11 +32,28 @@ const fetchCoordsByIP = (ip, callback) => {
   });
 };
 
-// const fetchISSFlyOverTimes = function(coords, callback) {
+const fetchISSFlyOverTimes = function(coords, callback) {
 
-// };
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, function(error, response, body) {
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const time = JSON.parse(body);
+    
+    callback(null, time.response);
+  });
+};
 
 module.exports = {
   fetchMyIP,
-  fetchCoordsByIP
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes
 };
